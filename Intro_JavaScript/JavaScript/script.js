0a82039ff3943322d4b452ab6bf9dd96ad9aac4a
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-04-21 10:14:22 +0200

    proj: First JavaScript tutorial

---
+++ Intro_JavaScript/JavaScript/script.js
@@ -0,0 +1,23 @@
+console.log( 40 + 8 )
+
+let javascipt = 3.5 
+
+console.log( javascipt)
+
+console.log(typeof true)
+
+year = 2013
+
+console.log(year)
+
+let age = 33
+age = 44
+
+console.log(age)
+
+var job = 'Programmer'
+job = 'Teacher'
+
+console.log(job)
+
+//let, const, var
