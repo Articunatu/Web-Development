29f8132d8bf83b0b881bf7d40ba5e9532412e1dd
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-04-28 11:44:33 +0200

    feat: JSON

--- 02-Dynamic CV/JavaScript/function.js
+++ 02-Dynamic CV/JavaScript/function.js
@@ -7,7 +7,96 @@ const jobs = [
 
 const educations = [
     {name: "Molekylärbiologi - Göteborgs Universitet",
-     timePeriod: "2014-2017"},
+        timePeriod: "2014-2017"},
     {name: "Elektroingenjör - Chalmers",
-     timePeriod: "2012-2014"},
+        timePeriod: "2012-2014"},
 ]
+
+document.getElementById('button').addEventListener('click', laddaData);
+
+
+function loadData(){
+    const xml = new XMLHttpRequest();
+    xml.open('GET', 'data.txt', true)
+
+    xml.onload = function(){
+        if(this.status===200 && this.readyState=== 4){
+            console.log(this.responseText);
+        }
+    }
+    xml.send();
+}
+
+function loadEducations(){
+    const xml = new XMLHttpRequest();
+    xml.open('GET', 'educations.json', true)
+
+    xml.onload = function(){
+        if(this.status===200){
+            const educations = JSON.parse(this.responseText);
+            let output = '';
+            educations.forEach(function(education){
+                output+=`
+                <ul>
+                    <li>${educations.name}</li>
+                    <li>${educations.timePeriod}</li>
+                </ul>
+                `;
+            }); 
+            document.getElementById('education').innerHTML = output;
+        }
+    }
+    xml.send();
+}
+
+function loadJobs(){
+    const xml = new XMLHttpRequest();
+    xml.open('GET', 'jobs.json', true)
+
+    xml.onload = function(){
+        if(this.status===200){
+            const jobs = JSON.parse(this.responseText);
+            let output = '';
+            jobs.forEach(function(education){
+                output+=`
+                <ul>
+                    <li>${jobs.name}</li>
+                    <li>${jobs.timePeriod}</li>
+                </ul>
+                `;
+            }); 
+            document.getElementById('education').innerHTML = output;
+        }
+    }
+    xml.send();
+}
+
+document.getElementById('button1').addEventListener('click', getApi);
+function getApi(){
+    fetch('https://api.github.com/users')
+    .then(function(res){
+    return res.json();
+    })
+    .then(function(data){
+    let output = '';
+    data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;   
+    });
+    document.getElementById('output').innerHTML = output;
+    })
+}
+
+document.getElementById('button1').addEventListener('click', getApi);
+function getApi(){
+    fetch('https://github.com/Articunatu?tab=repositories')
+    .then(function(res){
+    return res.json();
+    })
+    .then(function(data){
+    let output = '';
+    data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;   
+    });
+    document.getElementById('output').innerHTML = output;
+    })
+}
