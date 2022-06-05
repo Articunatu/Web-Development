706296c6a5f5c0cac157627ccbc0eef26a289923
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-06-05 16:49:50 +0200

    proj: New version of resume in react

---
+++ 03-Resume/src/components/Experience.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import {Link} from "react-router-dom";
+import Educations from "../JSON/educations.json";
+import Jobs from "../JSON/jobs.json";
+
+export default function Experience() 
+{
+    return ( 
+        <div className="App">
+            <div class="Fullname">
+                <h1>Christoffer Brandt</h1>
+                <h3>- Biokemist och Molekylärbiolog</h3>
+            </div>
+
+            <nav>
+                <ul className="Menu">
+                    <li ><Link to="/web-development/">Presentation</Link> </li>
+                    <li>Erfarenheter</li>
+                    <li><Link to="/web-development/portfolio">Portfolio</Link> </li>
+                    <li><Link to="/web-development/contacts">Kontakter</Link> </li>
+                </ul>
+            </nav>
+
+            <main>
+                <h2>Arbeten</h2>
+                {
+                    Jobs.map((Job) => {
+                        return (
+                            <div>
+                                <h3>{Job.namn}</h3>
+                                <p>{Job.tid}</p>
+                            </div>
+                        );
+                    })
+                }
+                <br />
+                <h2>Utbildning</h2>
+                {
+                    Educations.map((Education) => {
+                        return (
+                            <div>
+                                <h3>{Education.namn}</h3>
+                                <p>{Education.tid}</p>
+                            </div>
+                        );
+                    })
+                }
+            
+            </main>
+            <br />
+            <a> <img src="src/Images/lundby.jfif" alt="Sjukhus"/> </a>
+        </div>
+    );
+}
---
+++ 03-Resume/src/components/Home.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import {Link} from "react-router-dom";
+import { Code }  from '../JavaScript/Code'
+
+export default function Home() {
+    return (
+        <div>
+            <div class="Fullname">
+                <h1>Christoffer Brandt</h1>
+                <h3>- Biokemist och Molekylärbiolog</h3>
+            </div>
+            <nav>
+                <ul className="Menu">
+                    <li>Presentation</li>
+                    <li><Link to="/web-development/expeience">Erfarenheter</Link></li>
+                    <li><Link to="/web-development/portfolio">Portfolio</Link> </li>
+                    <li><Link to="/web-development/contacts">Kontakter</Link> </li>
+                </ul>
+            </nav>
+
+            <main>
+
+            <div class = "Introduction_A">
+                <h3>Vem jag är</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est deserunt autem consequuntur minima beatae quaerat facilis deleniti sequi, facere laboriosam asperiores ut pariatur fugiat dolores? Eligendi amet error maiores dignissimos.</p>
+            </div>
+
+            <div class = "Introduction_B">
+                <h3>Mina mål</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, saepe nobis earum assumenda asperiores recusandae? Nostrum quidem commodi, praesentium, optio ut quisquam porro, fuga earum atque ea temporibus laboriosam itaque
+                <div className="SecretButton" onClick={BackgroundSwap}>?</div> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aut velit consequatur atque sit rem sed illum dolore voluptates. Praesentium possimus dignissimos autem officiis laborum cumque omnis doloribus quam deleniti!
+                </p>
+            </div>
+            <a> <img src="src/Images/profil.png" alt="Profilbild"/> </a>
+            </main>
+        </div>
+    )
+}
+
+
+function BackgroundSwap() {
+    document.body.style.backgroundColor = "blue"
+}
